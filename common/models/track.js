f74baf7aedc411c818302a8136eded01ab69e17f
Dilter <dilter@outlook.com>
2015-07-24 14:45:27 -0300

    create start method on track model

--- common/models/track.js
+++ common/models/track.js
@@ -1,3 +1,37 @@
 module.exports = function(Track) {
 
+    Track.start = function(trackId, cb){
+
+        console.log(trackId);
+
+        var onFindTrack = function(err, track){
+
+            if(err)
+                cb(err);
+
+            track.status = 'started';
+            track.startedAt = Date.now();
+
+            track.save(function(err, obj){
+               cb(err, obj);
+            });
+        };
+
+        Track.findById(trackId, onFindTrack);
+
+    };
+
+    Track.remoteMethod('start', {
+
+        accepts: [
+            { arg: 'trackId', type: 'string', http:{ source: 'path' }}
+        ],
+        returns: {
+            type: 'object'
+        },
+        http: {
+            path: '/start/:trackId',
+            verb: 'put'
+        }
+    });
 };
